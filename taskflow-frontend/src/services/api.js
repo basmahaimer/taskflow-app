28c4016a352fe17b0f120b9595e13d0cce331d43
basmahaimer <basma.wija2012@gmail.com>
2025-09-07 02:19:36 +0100

    Add environment variables for frontend

--- taskflow-frontend/src/services/api.js
+++ taskflow-frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api", // ton backend Laravel
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api",
+  headers: {
+    "Content-Type": "application/json", // obligatoire pour que Laravel lise le JSON
+    Accept: "application/json",          // obligatoire pour que Laravel renvoie JSON sur erreur
+  },
 });
 
 // Intercepteur pour inclure automatiquement le token si présent
